da21ac7533d7ee6a4e575f3d4d3db11fc6a2139d
Rhymer-Lcy <1014917866@qq.com>
2025-08-11 10:18:56 +0800

    [Fix] update import path for checkResourceAllowed to ensure correct module resolution.

--- src/examples/server/simpleStreamableHttp.ts
+++ src/examples/server/simpleStreamableHttp.ts
@@ -9,7 +9,7 @@ import { CallToolResult, GetPromptResult, isInitializeRequest, PrimitiveSchemaDe
 import { InMemoryEventStore } from '../shared/inMemoryEventStore.js';
 import { setupAuthServer } from './demoInMemoryOAuthProvider.js';
 import { OAuthMetadata } from '../../shared/auth.js';
-import { checkResourceAllowed } from 'src/shared/auth-utils.js';
+import { checkResourceAllowed } from '../../shared/auth-utils.js';
 
 import cors from 'cors';
 
